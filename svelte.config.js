e18f4c6b6ae4a5fdc27d242176b51d9828b316bb
crlotwhite <crlotwhite@gmail.com>
2025-05-28 13:57:29 +0900

    Update deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -11,12 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		
-		// Add base path for GitHub Pages (repo name)
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/svelte-pianoroll' : ''
-		}
+		adapter: adapter()
 	}
 };
 
